7026d081dffbace18af59445422d0aa31f0d7870
Kreslav <kreslav@gmail.com>
2022-07-09 13:28:16 +0300

    Оставляю чистый проект!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Начинаю курсовую работу");
-        System.out.println("Установлено соединение с GitHub для контроля версий");
+        System.out.println("Оставляю чистый проект");
     }
 }
